7576750d7e183ce11e70f586c663de3c1e544100
webee.yw <webee.yw@gmail.com>
2016-04-07 17:38:56 +0800

    add comments for package libs/bytes/writer.

--- libs/bytes/writer.go
+++ libs/bytes/writer.go
@@ -5,22 +5,27 @@ type Writer struct {
 	buf []byte
 }
 
+// NewWriterSize returns a size n writer.
 func NewWriterSize(n int) *Writer {
 	return &Writer{buf: make([]byte, n)}
 }
 
+// Size returns writer's size.
 func (w *Writer) Size() int {
 	return len(w.buf)
 }
 
+// Reset reset writer's n.
 func (w *Writer) Reset() {
 	w.n = 0
 }
 
+// Buffer returns writer's current byte array.
 func (w *Writer) Buffer() []byte {
 	return w.buf[:w.n]
 }
 
+// Peek set writer's cursor to next n bytes and returns skipped bytes.
 func (w *Writer) Peek(n int) []byte {
 	var buf []byte
 	w.grow(n)
@@ -29,11 +34,13 @@ func (w *Writer) Peek(n int) []byte {
 	return buf
 }
 
+// Write append p to writer's buf.
 func (w *Writer) Write(p []byte) {
 	w.grow(len(p))
 	w.n += copy(w.buf[w.n:], p)
 }
 
+// grow extent writer's buf size to 2*cur_size + n if needed.
 func (w *Writer) grow(n int) {
 	var buf []byte
 	if w.n+n < len(w.buf) {
