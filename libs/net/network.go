4c8b9633331e96580b06386024b9a1d008ff765a
webee.yw <webee.yw@gmail.com>
2016-04-07 10:32:32 +0800

    change const name and add comment for exported func.

--- libs/net/network.go
+++ libs/net/network.go
@@ -6,16 +6,17 @@ import (
 )
 
 const (
-	networkSpliter = "@"
+	networkDelimiter = "@"
 )
 
+// ParseNetwork parse network@(host|ip):port from str.
 func ParseNetwork(str string) (network, addr string, err error) {
-	if idx := strings.Index(str, networkSpliter); idx == -1 {
-		err = fmt.Errorf("addr: \"%s\" error, must be network@tcp:port or network@unixsocket", str)
-		return
-	} else {
-		network = str[:idx]
-		addr = str[idx+1:]
+	idx := strings.Index(str, networkDelimiter)
+	if idx == -1 {
+		err = fmt.Errorf("addr: \"%s\" error, must be network@(host|ip):port or network@unixsocket", str)
 		return
 	}
+	network = str[:idx]
+	addr = str[idx+1:]
+	return
 }
